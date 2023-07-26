6944a230bf7e77355cc5b0951c740812dfe6b17c
Juan Francisco Markan <jfmarkan@gmail.com>
2023-07-26 19:24:45 +0200

    minor refurbish to code

--- resources/views/admin/comics/create.blade.php
+++ resources/views/admin/comics/create.blade.php
@@ -2,15 +2,20 @@
 
 @section('title', 'Admin Add New')
 
+@section('custom-stylesheets')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+@endsection
+
 @section('main-content')
     <div class="container">
         <div class="row justify-content-around">
-            <div class="col-8 bg-light">
+            <div class="col-8 bg-white my-5">
                 <h1>
                     Add a new Comic
                 </h1>
             </div>
-            <form class="col-8 bg-light" action="{{ route('admin.comics.store') }}" method="POST">
+
+            <form class="col-8 bg-light p-3 rounded" action="{{ route('admin.comics.store') }}" method="POST">
                 @csrf
                 <div class="mb-3">
                     <label for="title" class="form-label">
@@ -30,29 +35,31 @@
                     </label>
                     <input type="text" class="form-control" id="thumb" name="thumb">
                 </div>
-                <div class="mb-3">
-                    <label for="price" class="form-label">
-                        Price
-                    </label>
-                    <input type="text" class="form-control" id="price" name="price">
-                </div>
-                <div class="mb-3">
-                    <label for="series" class="form-label">
-                        Series
-                    </label>
-                    <input type="text" class="form-control" id="series" name="series">
-                </div>
-                <div class="mb-3">
-                    <label for="sale_date" class="form-label">
-                        Sale date:
-                    </label>
-                    <input type="text" class="form-control" id="sale_date" name="sale_date">
-                </div>
-                <div class="mb-3">
-                    <label for="type" class="form-label">
-                        Type
-                    </label>
-                    <input type="text" class="form-control" id="type" name="type">
+                <div class="d-flex justify-content-between">
+                    <div class="mb-3">
+                        <label for="price" class="form-label">
+                            Price
+                        </label>
+                        <input type="text" class="form-control" id="price" name="price">
+                    </div>
+                    <div class="mb-3">
+                        <label for="series" class="form-label">
+                            Series
+                        </label>
+                        <input type="text" class="form-control" id="series" name="series">
+                    </div>
+                    <div class="mb-3">
+                        <label for="sale_date" class="form-label">
+                            Sale date:
+                        </label>
+                        <input type="text" class="form-control" id="sale_date" name="sale_date">
+                    </div>
+                    <div class="mb-3">
+                        <label for="type" class="form-label">
+                            Type
+                        </label>
+                        <input type="text" class="form-control" id="type" name="type">
+                    </div>
                 </div>
                 <div class="mb-3">
                     <label for="artists" class="form-label">
@@ -67,10 +74,10 @@
                     <input type="text" class="form-control" id="writers" name="writers">
                 </div>
                 <button type="submit" class="btn btn-success">
-                    Add Comic
+                    <i class="fa-solid fa-check"></i>
                 </button>
                 <button type="reset" class="btn btn-danger">
-                    Clear fields
+                    <i class="fa-solid fa-xmark"></i>
                 </button>
             </form>
         </div>
